b186a09e1785f988f257460321eea034355a8ef2
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2022-11-28 19:35:33 +0100

    -adding some logic to form compt (css not setted yet)

--- src/component/Layout/Header/Form.js
+++ src/component/Layout/Header/Form.js
@@ -3,18 +3,20 @@ import React, { useState } from "react"
 const  Form = ()=>{
     const [data,setData] = useState({
         departure:"partenza",
-        arrive:"arriva"})
+        arrive:"arriva",
+        passengers:"1"})
     
     const submit = (e)=>{
         e.preventDefault()
-        console.log(data.departure,data.arrive)
+        console.log(data.departure,data.arrive,data.passengers)
     }
     return(
         <>
-            <form onSubmit={submit}>
+            <form className="flex flex-row justify-between" onSubmit={submit}>
                 <div>Dove vuoi andare?</div>
-                <input type="text" name="arrive" onChange={(e)=>setData({arrive: e.target.value})}/>
-                <input type="text" name="departure" onChange={(e)=>setData({departure: e.target.value})}/>
+                <input type="text" name="arrive" onChange={(e)=>setData({...data, departure: e.target.value})}/>
+                <input type="text" name="departure" onChange={(e)=>setData({...data, arrive: e.target.value})}/>
+                <input type="text" name="passenger" onChange={(e)=>setData({...data,passenger: e.target.value})}/>
                 <button type="submit">SUBMIT</button>
             </form>
             
