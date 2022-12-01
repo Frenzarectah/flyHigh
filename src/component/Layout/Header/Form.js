b0c9f3e8120eabfce0472a957fecf926ad227c69
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2022-12-01 23:44:40 +0100

    added the customized favicon

--- src/component/Layout/Header/Form.js
+++ src/component/Layout/Header/Form.js
@@ -12,11 +12,25 @@ const  Form = ()=>{
     }
     return(
         <>
-            <form className="flex flex-row justify-between" onSubmit={submit}>
+            <form className="flex flex-col justify-center items-center h-[75%]" onSubmit={submit}>
                 <div>Dove vuoi andare?</div>
-                <input type="text" name="arrive" onChange={(e)=>setData({...data, departure: e.target.value})}/>
-                <input type="text" name="departure" onChange={(e)=>setData({...data, arrive: e.target.value})}/>
-                <input type="text" name="passenger" onChange={(e)=>setData({...data,passenger: e.target.value})}/>
+                <div className="flex flex-col justify-center items-center md:flex-row">
+                    <input placeholder="Partenza" 
+                        className="w-[75%] m-1" 
+                        type="text" 
+                        name="departure" 
+                        onChange={(e)=>setData({...data, departure: e.target.value})}/>
+                    <input placeholder="Arrivo"
+                        className="w-[75%] m-1" 
+                        type="text" 
+                        name="arrive"
+                        onChange={(e)=>setData({...data, arrive: e.target.value})}/>
+                    <input placeholder="Passeggeri" 
+                        className="w-[75%] m-1" 
+                        type="text" 
+                        name="passenger" 
+                        onChange={(e)=>setData({...data,passenger: e.target.value})}/>
+                </div>        
                 <button type="submit">SUBMIT</button>
             </form>
             
--- src/component/Layout/Header/Header.js
+++ src/component/Layout/Header/Header.js
@@ -4,7 +4,7 @@ import Form from "./Form"
 const Header = () => {
     return(
         <header className="w-full h-96 flex justify-center items-center bg-header bg-no-repeat bg-cover border-1 border-black">
-            <div className="w-1/2 h-1/2 bg-[#dcdcdc] rounded">
+            <div style={{"background":"rgba(39, 245, 238, 0.82)"}} className="w-1/2 h-1/2 bg-[#dcdcdc] rounded">
                 <Form/>
             </div>
         </header>
