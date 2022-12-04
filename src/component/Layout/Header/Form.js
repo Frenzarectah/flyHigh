1383c17fff83f73ab75112893e453f841bd4b7e1
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2022-12-05 00:03:29 +0100

    implementing states on form component

--- src/component/Layout/Header/Form.js
+++ src/component/Layout/Header/Form.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
-
+import logo from '../../../asset/logo_opacity0.png'
 const  Form = ()=>{
-    const [select,setSelect] = useState("1")
     const [data,setData] = useState({
         departure:"partenza",
         arrive:"arriva",
@@ -9,13 +8,12 @@ const  Form = ()=>{
     
     const submit = (e)=>{
         e.preventDefault()
-        console.log(select)
-        setData(...data, { passenger : select} )
         console.log(data.departure,data.arrive,data.passengers)
     }
     return(
         <>
             <form className="flex flex-col justify-center items-center h-[75%]" onSubmit={submit}>
+                <img className="absolute top-2 right-2" alt="main logo" src={logo}/>
                 <div>Dove vuoi andare?</div>
                 <div className="flex flex-col justify-center items-center md:flex-row">
                     <input placeholder="Partenza" 
@@ -30,15 +28,15 @@ const  Form = ()=>{
                         onChange={(e)=>setData({...data, arrive: e.target.value})}/>
                     <select placeholder="Passeggeri" 
                         className="w-[75%] m-1"
-                        value={select}
+                        value={data.passengers}
                         name="passenger"
-                        onChange={(e)=>setSelect(e.target.value)}>
+                        onChange={(e)=>setData({...data, passengers: e.target.value})}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                     </select>
                 </div>        
-                <button type="submit">SUBMIT</button>
+                <button className="w-fit h-fit bg-[#ff0000] radius rounded text-white" type="submit">SUBMIT</button>
             </form>
             
         </>
