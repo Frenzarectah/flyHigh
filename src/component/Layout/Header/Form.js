5570316be2fcb00bead8b570e18ea7ac9d6cb071
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-12-05 17:31:03 +0100

    added date fields to the form

--- src/component/Layout/Header/Form.js
+++ src/component/Layout/Header/Form.js
@@ -8,35 +8,49 @@ const  Form = ()=>{
     
     const submit = (e)=>{
         e.preventDefault()
-        console.log(data.departure,data.arrive,data.passengers)
+        console.log(data.departure,data.arrive,data.passengers,data.backData,data.departData)
     }
     return(
         <>
-            <form className="flex flex-col justify-center items-center h-[75%]" onSubmit={submit}>
+            <form className="flex flex-col justify-center items-center h-full md:h-[75%]" onSubmit={submit}>
                 <img className="absolute top-2 right-2" alt="main logo" src={logo}/>
                 <div>Dove vuoi andare?</div>
-                <div className="flex flex-col justify-center items-center md:flex-row">
-                    <input placeholder="Partenza" 
-                        className="w-[75%] m-1" 
-                        type="text" 
-                        name="departure" 
-                        onChange={(e)=>setData({...data, departure: e.target.value})}/>
-                    <input placeholder="Arrivo"
-                        className="w-[75%] m-1" 
-                        type="text" 
-                        name="arrive"
-                        onChange={(e)=>setData({...data, arrive: e.target.value})}/>
-                    <select placeholder="Passeggeri" 
-                        className="w-[75%] m-1"
-                        value={data.passengers}
-                        name="passenger"
-                        onChange={(e)=>setData({...data, passengers: e.target.value})}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                    </select>
+                <div>
+                    <div className="flex flex-col justify-center items-center md:flex-row">
+                        <input placeholder="Partenza" 
+                            className="w-[75%] m-1" 
+                            type="text" 
+                            name="departure" 
+                            onChange={(e)=>setData({...data, departure: e.target.value})}/>
+                        <input placeholder="Arrivo"
+                            className="w-[75%] m-1" 
+                            type="text" 
+                            name="arrive"
+                            onChange={(e)=>setData({...data, arrive: e.target.value})}/>
+                        <select placeholder="Passeggeri" 
+                            className="w-[75%] m-1"
+                            value={data.passengers}
+                            name="passenger"
+                            onChange={(e)=>setData({...data, passengers: e.target.value})}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                    <div className="justify-center items-center flex flex-col md:flex-row">
+                        <label for="andata">andata</label>
+                        <input type="date"
+                            id="andata"
+                            className="w-[75%] md:w-[30%] m-1" 
+                            onChange={(e)=>setData({...data, departData: e.target.value})}/>
+                        <label for="ritorno">ritorno</label>
+                        <input type="date"
+                            id="ritorno"
+                            className="w-[75%] md:w-[30%] m-1" 
+                            onChange={(e)=>setData({...data, backData: e.target.value})}/>
+                    </div>
                 </div>        
-                <button className="w-fit h-fit bg-[#ff0000] radius rounded text-white" type="submit">SUBMIT</button>
+                <button className="w-20 h-7 bg-[#008dd5] radius rounded text-white" type="submit">Let's Fly!</button>
             </form>
             
         </>
