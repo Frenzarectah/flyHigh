09249827b4c681af19bb7136b0717c75d2caadb8
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-12-14 17:27:46 +0100

    experiment with useReducer hook [WIP]

--- src/component/Layout/Header/Header.js
+++ src/component/Layout/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <header className="w-full h-96 flex justify-center items-center bg-header bg-no-repeat bg-cover border-1 border-black">
             <div style={{"background":"rgba(39, 245, 238, 0.82)"}} className="w-1/2 h-1/2 bg-[#dcdcdc] rounded">
                 <Form handlerAPI={()=>returnData()}/>
-                {data}
+                {data.flight_iata}
             </div>
         </header>
     )
