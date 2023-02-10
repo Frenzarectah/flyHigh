033c92a9ae6e28fe61ada9cb4f1ffb6f29d01ee8
Frenzarectah <sorrentino.francesco87@gmail.com>
2023-02-10 17:03:17 +0100

    added an API call

--- src/component/Layout/ApiRenderer.js
+++ src/component/Layout/ApiRenderer.js
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-const ApiRenderer = (props)=>{
+const ApiRenderer = ({props})=>{
+    const {departure,arrive} = props
+    const [response,setResponse] = useState({})
+    useEffect(()=>{
+        fetch(`https://airlabs.co/api/v9/schedules?dep_iata=${departure}&arr_iata=${arrive}&api_key=b4137d54-e61a-46d8-967a-33ecc5836d83`)
+            .then((response) => response.json())
+            .then((data)=> setResponse(data.response[0]))
+    },[departure,arrive,props])
     return(
-        <h1>{props.arrive}</h1>
+        <h1>{response.flight_iata}</h1>
     )
 }
 
